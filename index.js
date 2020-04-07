daedb83b7f21d1c2d931b0f2c48149e3082a2b87
Phyrex Tsai <x01ep23i@hotmail.com>
2020-04-07 12:07:28 +0800

    Update index.js add contracts

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const PublicResolver = require('./build/contracts/PublicResolver.json')
 const Resolver = require('./build/contracts/Resolver.json')
+const ECNS = require('./build/contracts/ECNS.json')
+const ECNSRegistry = require('./build/contracts/ECNSRegistry.json')
 
 module.exports = {
+  ECNS,
+  ECNSRegistry,
   PublicResolver,
   Resolver
 }
