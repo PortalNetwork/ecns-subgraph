dfd358216407a614c56c71243ba12393216da3bd
Phyrex Tsai <x01ep23i@hotmail.com>
2020-04-07 11:34:49 +0800

    Add index.js

---
+++ contracts/StablePriceOracle.sol
@@ -0,0 +1,41 @@
+pragma solidity >=0.4.24;
+
+import "./PriceOracle.sol";
+import "./SafeMath.sol";
+import "./StringUtils.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+interface DSValue {
+  function read() external view returns (bytes32);
+}
+
+contract StablePriceOracle is Ownable, PriceOracle {
+  using SafeMath for *;
+  using StringUtils for *;
+
+  DSValue usdOracle;
+
+  uint[] public rentPrices;
+
+  event OracleChanged(address oracle);
+  event RentPriceChanged(uint[] prices);
+
+  constructor(DSValue _usdOracle, uint[] memory _rentPrices) public {
+    setOracle(_usdOracle);
+    setPrices(_rentPrices);
+  }
+
+  function setOracle(DSValue _usdOracle) public onlyOwner {
+    usdOracle = _usdOracle;
+    emit OracleChanged(address(_usdOracle));
+  }
+
+  function setPrices(uint[] memory _rentPrices) public onlyOwner {
+    rentPrices = _rentPrices;
+    emit RentPriceChanged(_rentPrices);
+  }
+
+  function price(string calldata name, uint /*expires*/, uint duration) view external returns(uint) {
+
+  }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+const PublicResolver = require('./build/contracts/PublicResolver.json')
+const Resolver = require('./build/contracts/Resolver.json')
+
+module.exports = {
+  PublicResolver,
+  Resolver
+}
